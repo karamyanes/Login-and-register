2aa1de17e3b7978c3e8cf6294a2b710266a82951
Eslam Elgamal <me@esl4m.com>
2021-01-16 16:31:14 +0300

    Adds models and migrations

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Type(models.Model):
+    type = (
+        (1, "Expense"),
+        (2, "Income"),
+    )
+    date = models.DateTimeField(auto_now_add=True, blank=False)
+
+    def __str__(self):
+        return self.type
+
+
+class Transaction(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=256)
+    amount = models.FloatField(null=False, blank=False, default=None)
+    type = models.IntegerField(choices=Type.type, default=1, blank=False)  # added Expense size as default
+
+    def __str__(self):
+        return self.name
