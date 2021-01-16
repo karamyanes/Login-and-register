f9ed416ac14a4d46f907e1597d303604a8b6db07
Eslam Elgamal <me@esl4m.com>
2021-01-16 14:22:29 +0300

    Adds users app urls to access endpoints

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.urls import path, include
+from .api import RegisterApi, UserLogoutAll, CustomTokenObtainPairView
+from rest_framework_simplejwt import views as jwt_views
+
+
+urlpatterns = [
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', UserLogoutAll.as_view(), name='logout'),
+    path('register', RegisterApi.as_view()),
+]
