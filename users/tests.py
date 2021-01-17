2166b4b94aa4fc3aa2ca3d88a80f6c6a2b40786d
Eslam Elgamal <me@esl4m.com>
2021-01-17 17:53:53 +0300

    Inits simple test case for Login and Register in users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class LoginTest(TestCase):
+    """
+    Test module for Login
+    """
+    def setUp(self):
+        self.base_url = 'http://0.0.0.0:3000/api/users/login/'
+        self.client = Client()
+
+    def test_login(self):
+        """ Test Login """
+        response = self.client.get(self.base_url)
+        # Test success listing
+        self.assertEqual(response.status_code, 200)
+
+
+class RegisterTest(TestCase):
+    """
+    Test module for Register
+    """
+    def setUp(self):
+        self.base_url = 'http://0.0.0.0:3000/api/users/register/'
+        self.client = Client()
+
+    def test_register(self):
+        """ Test Register """
+        response = self.client.get(self.base_url)
+        # Test success listing
+        self.assertEqual(response.status_code, 200)
